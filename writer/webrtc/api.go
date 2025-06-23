1882c2a051e610afeceaaed62f7621ed1dd3bf23
Gleb Shleikel <z1rachl@yandex.ru>
2025-06-23 13:11:55 +0300

    Enhance WebRTC audio and video codec support
    This commit introduces several new audio and video codecs to the WebRTC implementation, including:
    - Added support for Opus, G722, PCMU, and PCMA audio codecs.
    - Enhanced video codec registration with support for VP8, H264, and VP9, including various payload types and RTCP feedback configurations.
    - Improved error handling during codec registration to ensure robust initialization.
    
    These changes significantly expand the codec capabilities of the WebRTC framework, improving compatibility and performance for real-time streaming.

--- writer/webrtc/api.go
+++ writer/webrtc/api.go
@@ -2,6 +2,7 @@ package webrtc
 
 import (
 	"github.com/pion/interceptor"
+	"github.com/pion/rtp"
 	"github.com/pion/webrtc/v4"
 	"github.com/ugparu/gomedia/utils/logger"
 )
@@ -23,87 +24,374 @@ func Init(minPort, maxPort uint16, hosts []string, iceServers []webrtc.ICEServer
 		SDPSemantics:         webrtc.SDPSemanticsUnifiedPlanWithFallback,
 	}
 
-	// Create a new MediaEngine
-	m := &webrtc.MediaEngine{}
-	if err := m.RegisterDefaultCodecs(); err != nil {
+	m := new(webrtc.MediaEngine)
+
+	// Default Pion Audio Codecs
+	for _, codec := range []webrtc.RTPCodecParameters{
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeOpus,
+				ClockRate:    48000,
+				Channels:     2,
+				SDPFmtpLine:  "minptime=10;useinbandfec=1",
+				RTCPFeedback: nil,
+			},
+			PayloadType: 111,
+		},
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeG722,
+				ClockRate:    8000,
+				Channels:     0,
+				SDPFmtpLine:  "",
+				RTCPFeedback: nil,
+			},
+			PayloadType: rtp.PayloadTypeG722,
+		},
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypePCMU,
+				ClockRate:    8000,
+				Channels:     0,
+				SDPFmtpLine:  "",
+				RTCPFeedback: nil,
+			},
+			PayloadType: rtp.PayloadTypePCMU,
+		},
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypePCMA,
+				ClockRate:    8000,
+				Channels:     0,
+				SDPFmtpLine:  "",
+				RTCPFeedback: nil,
+			},
+			PayloadType: rtp.PayloadTypePCMA,
+		},
+	} {
+		if err := m.RegisterCodec(codec, webrtc.RTPCodecTypeAudio); err != nil {
+			logger.Fatal("WEBRTC", err.Error())
+		}
+	}
+
+	videoRTCPFeedback := []webrtc.RTCPFeedback{{Type: "goog-remb", Parameter: ""}, {Type: "ccm", Parameter: "fir"}, {Type: "nack", Parameter: ""}, {Type: "nack", Parameter: "pli"}}
+	for _, codec := range []webrtc.RTPCodecParameters{
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeVP8,
+				ClockRate:    90000,
+				Channels:     0,
+				SDPFmtpLine:  "",
+				RTCPFeedback: videoRTCPFeedback,
+			},
+			PayloadType: 96,
+		},
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeRTX,
+				ClockRate:    90000,
+				Channels:     0,
+				SDPFmtpLine:  "apt=96",
+				RTCPFeedback: nil,
+			},
+			PayloadType: 97,
+		},
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeH264,
+				ClockRate:    90000,
+				Channels:     0,
+				SDPFmtpLine:  "level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42001f",
+				RTCPFeedback: videoRTCPFeedback,
+			},
+			PayloadType: 102,
+		},
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeRTX,
+				ClockRate:    90000,
+				Channels:     0,
+				SDPFmtpLine:  "apt=102",
+				RTCPFeedback: nil,
+			},
+			PayloadType: 103,
+		},
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeH264,
+				ClockRate:    90000,
+				Channels:     0,
+				SDPFmtpLine:  "level-asymmetry-allowed=1;packetization-mode=0;profile-level-id=42001f",
+				RTCPFeedback: videoRTCPFeedback,
+			},
+			PayloadType: 104,
+		},
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeRTX,
+				ClockRate:    90000,
+				Channels:     0,
+				SDPFmtpLine:  "apt=104",
+				RTCPFeedback: nil,
+			},
+			PayloadType: 105,
+		},
+
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeH264,
+				ClockRate:    90000,
+				Channels:     0,
+				SDPFmtpLine:  "level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42e01f",
+				RTCPFeedback: videoRTCPFeedback,
+			},
+			PayloadType: 106,
+		},
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeRTX,
+				ClockRate:    90000,
+				Channels:     0,
+				SDPFmtpLine:  "apt=106",
+				RTCPFeedback: nil,
+			},
+			PayloadType: 107,
+		},
+
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeH264,
+				ClockRate:    90000,
+				Channels:     0,
+				SDPFmtpLine:  "level-asymmetry-allowed=1;packetization-mode=0;profile-level-id=42e01f",
+				RTCPFeedback: videoRTCPFeedback,
+			},
+			PayloadType: 108,
+		},
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeRTX,
+				ClockRate:    90000,
+				Channels:     0,
+				SDPFmtpLine:  "apt=108",
+				RTCPFeedback: nil,
+			},
+			PayloadType: 109,
+		},
+
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeH264,
+				ClockRate:    90000,
+				Channels:     0,
+				SDPFmtpLine:  "level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=4d001f",
+				RTCPFeedback: videoRTCPFeedback,
+			},
+			PayloadType: 127,
+		},
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeRTX,
+				ClockRate:    90000,
+				Channels:     0,
+				SDPFmtpLine:  "apt=127",
+				RTCPFeedback: nil,
+			},
+			PayloadType: 125,
+		},
+
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeH264,
+				ClockRate:    90000,
+				Channels:     0,
+				SDPFmtpLine:  "level-asymmetry-allowed=1;packetization-mode=0;profile-level-id=4d001f",
+				RTCPFeedback: videoRTCPFeedback,
+			},
+			PayloadType: 39,
+		},
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeRTX,
+				ClockRate:    90000,
+				Channels:     0,
+				SDPFmtpLine:  "apt=39",
+				RTCPFeedback: nil,
+			},
+			PayloadType: 40,
+		},
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeH265,
+				ClockRate:    90000,
+				RTCPFeedback: videoRTCPFeedback,
+			},
+			PayloadType: 116,
+		},
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeRTX,
+				ClockRate:    90000,
+				Channels:     0,
+				SDPFmtpLine:  "apt=116",
+				RTCPFeedback: nil,
+			},
+			PayloadType: 117,
+		},
+		// {
+		// 	RTPCodecCapability: webrtc.RTPCodecCapability{
+		// 		MimeType:     webrtc.MimeTypeAV1,
+		// 		ClockRate:    90000,
+		// 		Channels:     0,
+		// 		SDPFmtpLine:  "",
+		// 		RTCPFeedback: videoRTCPFeedback,
+		// 	},
+		// 	PayloadType: 45,
+		// },
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     "video/h265",
+				ClockRate:    90000, //nolint:mnd // 90k
+				Channels:     0,
+				SDPFmtpLine:  "level-id=180;profile-id=1;tier-flag=0;tx-mode=SRST",
+				RTCPFeedback: videoRTCPFeedback,
+			},
+			PayloadType: 45, //nolint:mnd
+		},
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     "video/h265",
+				ClockRate:    90000, //nolint:mnd // 90k
+				Channels:     0,
+				SDPFmtpLine:  "level-id=180;profile-id=2;tier-flag=0;tx-mode=SRST",
+				RTCPFeedback: videoRTCPFeedback,
+			},
+			PayloadType: 47, //nolint:mnd
+		},
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeRTX,
+				ClockRate:    90000,
+				Channels:     0,
+				SDPFmtpLine:  "apt=45",
+				RTCPFeedback: nil,
+			},
+			PayloadType: 46,
+		},
+
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeVP9,
+				ClockRate:    90000,
+				Channels:     0,
+				SDPFmtpLine:  "profile-id=0",
+				RTCPFeedback: videoRTCPFeedback,
+			},
+			PayloadType: 98,
+		},
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeRTX,
+				ClockRate:    90000,
+				Channels:     0,
+				SDPFmtpLine:  "apt=98",
+				RTCPFeedback: nil,
+			},
+			PayloadType: 99,
+		},
+
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeVP9,
+				ClockRate:    90000,
+				Channels:     0,
+				SDPFmtpLine:  "profile-id=2",
+				RTCPFeedback: videoRTCPFeedback,
+			},
+			PayloadType: 100,
+		},
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeRTX,
+				ClockRate:    90000,
+				Channels:     0,
+				SDPFmtpLine:  "apt=100",
+				RTCPFeedback: nil,
+			},
+			PayloadType: 101,
+		},
+
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeH264,
+				ClockRate:    90000,
+				Channels:     0,
+				SDPFmtpLine:  "level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=64001f",
+				RTCPFeedback: videoRTCPFeedback,
+			},
+			PayloadType: 112,
+		},
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeRTX,
+				ClockRate:    90000,
+				Channels:     0,
+				SDPFmtpLine:  "apt=112",
+				RTCPFeedback: nil,
+			},
+			PayloadType: 113,
+		},
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeH265,
+				ClockRate:    90000, //nolint:mnd // 90k
+				Channels:     0,
+				SDPFmtpLine:  "level-id=93;profile-id=1;tier-flag=0;tx-mode=SRST",
+				RTCPFeedback: videoRTCPFeedback,
+			},
+			PayloadType: 116,
+		},
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeH265,
+				ClockRate:    90000, //nolint:mnd // 90k
+				Channels:     0,
+				SDPFmtpLine:  "level-id=93;profile-id=2;tier-flag=0;tx-mode=SRST",
+				RTCPFeedback: videoRTCPFeedback,
+			},
+			PayloadType: 118, //nolint:mnd
+		},
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeH265,
+				ClockRate:    90000, //nolint:mnd // 90k
+				Channels:     0,
+				SDPFmtpLine:  "level-id=180;profile-id=1;tier-flag=0;tx-mode=SRST",
+				RTCPFeedback: videoRTCPFeedback,
+			},
+			PayloadType: 123, //nolint:mnd
+		},
+		{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:     webrtc.MimeTypeH265,
+				ClockRate:    90000, //nolint:mnd // 90k
+				Channels:     0,
+				SDPFmtpLine:  "level-id=180;profile-id=2;tier-flag=0;tx-mode=SRST",
+				RTCPFeedback: videoRTCPFeedback,
+			},
+			PayloadType: 125, //nolint:mnd
+		},
+	} {
+		if err := m.RegisterCodec(codec, webrtc.RTPCodecTypeVideo); err != nil {
+			logger.Fatal("WEBRTC", err.Error())
+		}
+	}
+
+	if err := m.RegisterCodec(webrtc.RTPCodecParameters{}, webrtc.RTPCodecTypeVideo); err != nil {
 		logger.Fatal("WEBRTC", err.Error())
 	}
 
-	if err := m.RegisterCodec(webrtc.RTPCodecParameters{
-		RTPCodecCapability: webrtc.RTPCodecCapability{
-			MimeType:     webrtc.MimeTypeH265,
-			ClockRate:    90000, //nolint:mnd // 90k
-			Channels:     0,
-			SDPFmtpLine:  "level-id=93;profile-id=1;tier-flag=0;tx-mode=SRST",
-			RTCPFeedback: nil,
-		},
-		PayloadType: 116, //nolint:mnd
-	}, webrtc.RTPCodecTypeVideo); err != nil {
-		logger.Fatal("WEBRTC", err.Error())
-	}
-
-	if err := m.RegisterCodec(webrtc.RTPCodecParameters{
-		RTPCodecCapability: webrtc.RTPCodecCapability{
-			MimeType:     webrtc.MimeTypeH265,
-			ClockRate:    90000, //nolint:mnd // 90k
-			Channels:     0,
-			SDPFmtpLine:  "level-id=93;profile-id=2;tier-flag=0;tx-mode=SRST",
-			RTCPFeedback: nil,
-		},
-		PayloadType: 118, //nolint:mnd
-	}, webrtc.RTPCodecTypeVideo); err != nil {
-		logger.Fatal("WEBRTC", err.Error())
-	}
-
-	if err := m.RegisterCodec(webrtc.RTPCodecParameters{
-		RTPCodecCapability: webrtc.RTPCodecCapability{
-			MimeType:     webrtc.MimeTypeH265,
-			ClockRate:    90000, //nolint:mnd // 90k
-			Channels:     0,
-			SDPFmtpLine:  "level-id=180;profile-id=1;tier-flag=0;tx-mode=SRST",
-			RTCPFeedback: nil,
-		},
-		PayloadType: 123, //nolint:mnd
-	}, webrtc.RTPCodecTypeVideo); err != nil {
-		logger.Fatal("WEBRTC", err.Error())
-	}
-
-	if err := m.RegisterCodec(webrtc.RTPCodecParameters{
-		RTPCodecCapability: webrtc.RTPCodecCapability{
-			MimeType:     webrtc.MimeTypeH265,
-			ClockRate:    90000, //nolint:mnd // 90k
-			Channels:     0,
-			SDPFmtpLine:  "level-id=180;profile-id=2;tier-flag=0;tx-mode=SRST",
-			RTCPFeedback: nil,
-		},
-		PayloadType: 125, //nolint:mnd
-	}, webrtc.RTPCodecTypeVideo); err != nil {
-		logger.Fatal("WEBRTC", err.Error())
-	}
-
-	if err := m.RegisterCodec(webrtc.RTPCodecParameters{
-		RTPCodecCapability: webrtc.RTPCodecCapability{
-			MimeType:     "video/h265",
-			ClockRate:    90000, //nolint:mnd // 90k
-			Channels:     0,
-			SDPFmtpLine:  "level-id=180;profile-id=1;tier-flag=0;tx-mode=SRST",
-			RTCPFeedback: nil,
-		},
-		PayloadType: 45, //nolint:mnd
-	}, webrtc.RTPCodecTypeVideo); err != nil {
-		logger.Fatal("WEBRTC", err.Error())
-	}
-
-	if err := m.RegisterCodec(webrtc.RTPCodecParameters{
-		RTPCodecCapability: webrtc.RTPCodecCapability{
-			MimeType:     "video/h265",
-			ClockRate:    90000, //nolint:mnd // 90k
-			Channels:     0,
-			SDPFmtpLine:  "level-id=180;profile-id=2;tier-flag=0;tx-mode=SRST",
-			RTCPFeedback: nil,
-		},
-		PayloadType: 47, //nolint:mnd
-	}, webrtc.RTPCodecTypeVideo); err != nil {
+	if err := m.RegisterCodec(webrtc.RTPCodecParameters{}, webrtc.RTPCodecTypeVideo); err != nil {
 		logger.Fatal("WEBRTC", err.Error())
 	}
 
