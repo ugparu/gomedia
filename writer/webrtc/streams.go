9b045a190968c28a4ad1e1fb4d44a84a2bfc2eae
Gleb Shleikel <z1rachl@yandex.ru>
2025-08-19 09:39:51 +0300

    Update target duration in sortedStreams to 15 seconds for improved stream handling

--- writer/webrtc/streams.go
+++ writer/webrtc/streams.go
@@ -116,7 +116,7 @@ func (ss *sortedStreams) Add(url string, newCodecPar gomedia.CodecParameters) {
 		buffer: &Buffer{
 			gops:           nil,
 			duration:       0,
-			targetDuration: time.Minute,
+			targetDuration: time.Second * 15,
 		},
 		codecPar: pair,
 	}
