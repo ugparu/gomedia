717df247ddbd838dc730341e446002e6c649b203
Gleb Shleikel <gl.schleikel@gmail.com>
2026-03-25 12:19:32 +0300

    refactor: Enhance RTP ring buffer options by adding usage logging for better resource monitoring

--- format/rtsp/demuxer.go
+++ format/rtsp/demuxer.go
@@ -154,9 +154,10 @@ func (dmx *innerRTSPDemuxer) findStreams() (params gomedia.CodecParametersPair, 
 				logName = parsedURL.Host + "_" + logName
 			}
 
-			opts = append(opts, rtp.WithRingBuffer(dmx.rtpRingBufferSize, buffer.WithLogName(logName), buffer.WithStaleRingAllocFn(time.Minute, func(idx uint64, age time.Duration) {
-				dmx.log.Errorf(dmx.String(), "RTP ring alloc slot %d held for %v, leaking", idx, age)
-			})))
+			opts = append(opts, rtp.WithRingBuffer(dmx.rtpRingBufferSize, buffer.WithLogName(logName),
+				buffer.WithStaleRingAllocFn(time.Minute, func(idx uint64, age time.Duration) {
+					dmx.log.Errorf(dmx.String(), "RTP ring alloc slot %d held for %v, leaking", idx, age)
+				}), buffer.WithUsageLog(time.Minute)))
 		}
 		switch i2.AVType {
 		case video:
